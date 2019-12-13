28d2c0a176ae3b82c2c92e5c2c7b461a4c5efea3
thanya <32271882+ThanyaSanthosh@users.noreply.github.com>
2019-12-13 17:11:40 +0530

    added more tests
    What changes you have made?
    .....
    
    Why this change is necessary?
    ....
    
    What is the impact of the change?
    .....

--- test/testLine.js
+++ test/testLine.js
@@ -33,7 +33,7 @@ describe('Line', function() {
   });
 
   describe('length', function() {
-    it('should give length of the line', function() {
+    it('should give length of the line when both the points are not same and have integer value', function() {
       const myLine = new Line({ x: 2, y: 3 }, { x: 3, y: 3 });
       assert.strictEqual(myLine.length, 1);
     });
@@ -41,5 +41,11 @@ describe('Line', function() {
       const myLine = new Line({ x: 3, y: 3 }, { x: 3, y: 3 });
       assert.strictEqual(myLine.length, 0);
     });
+  });
+  it('should give length of the line when both the points are not same and have fractional value', function() {
+    let myLine = new Line({ x: 2, y: 3 }, { x: 3.5, y: 3 });
+    assert.strictEqual(myLine.length, 1.5);
+    myLine = new Line({ x: 2, y: 3 }, { x: 3.5, y: 4.5 });
+    assert.strictEqual(myLine.length, 2.1213203435596424);
   });
 });
