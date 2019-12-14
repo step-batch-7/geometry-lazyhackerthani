e702379023bafb4c12f76f299c3829e29fb9fe3c
thanya <32271882+ThanyaSanthosh@users.noreply.github.com>
2019-12-14 11:15:23 +0530

    refactored test(descriptions and it sentances)

--- test/testLine.js
+++ test/testLine.js
@@ -3,67 +3,67 @@ const assert = require('chai').assert;
 const Line = require('../src/line.js');
 
 describe('Line', function() {
-  describe('toString in Line class', function() {
-    it('should give features of a line', function() {
-      const myLine = new Line({ x: 2, y: 3 }, { x: 3, y: 4 });
-      const actual = myLine.toString();
+  describe('toString', function() {
+    it('should give string representation of a line', function() {
+      const line = new Line({ x: 2, y: 3 }, { x: 3, y: 4 });
+      const actual = line.toString();
       const expected = `[Line (2,3) to (3,4)]`;
       assert.deepStrictEqual(actual, expected);
     });
   });
 
-  describe('isEqualTo in Line class', function() {
-    it('should give true if given object is a instance of line have same properties', function() {
-      const myLine = new Line({ x: 2, y: 3 }, { x: 3, y: 4 });
-      const myNewLine = new Line({ x: 2, y: 3 }, { x: 3, y: 4 });
-      const actual = myLine.isEqualTo(myNewLine);
+  describe('isEqualTo', function() {
+    it('should give true if given object is a instance of line and have same values', function() {
+      const line = new Line({ x: 2, y: 3 }, { x: 3, y: 4 });
+      const otherLine = new Line({ x: 2, y: 3 }, { x: 3, y: 4 });
+      const actual = line.isEqualTo(otherLine);
       assert.isTrue(actual);
     });
     it('should give false if given object is a instance of line but have different values', function() {
-      const myLine = new Line({ x: 2, y: 3 }, { x: 3, y: 4 });
-      const myNewLine = new Line({ x: 2, y: 3 }, { x: 4, y: 4 });
-      const actual = myLine.isEqualTo(myNewLine);
+      const line = new Line({ x: 2, y: 3 }, { x: 3, y: 4 });
+      const otherLine = new Line({ x: 2, y: 3 }, { x: 4, y: 4 });
+      const actual = line.isEqualTo(otherLine);
       assert.isFalse(actual);
     });
     it('should give false if given object is not a instance of line', function() {
-      const myLine = new Line({ x: 2, y: 3 }, { x: 3, y: 4 });
-      const myNewLine = { x: 2, y: 3 };
-      const actual = myLine.isEqualTo(myNewLine);
+      const line = new Line({ x: 2, y: 3 }, { x: 3, y: 4 });
+      const otherLine = { x: 2, y: 3 };
+      const actual = line.isEqualTo(otherLine);
       assert.isFalse(actual);
     });
   });
 
   describe('length', function() {
     it('should give length of the line when both the points are not same and have integer value', function() {
-      const myLine = new Line({ x: 2, y: 3 }, { x: 3, y: 3 });
-      assert.strictEqual(myLine.length, 1);
+      const line = new Line({ x: 2, y: 3 }, { x: 3, y: 3 });
+      assert.strictEqual(line.length, 1);
     });
     it('should give length as 0 when the line is actually a point', function() {
-      const myLine = new Line({ x: 3, y: 3 }, { x: 3, y: 3 });
-      assert.strictEqual(myLine.length, 0);
+      const line = new Line({ x: 3, y: 3 }, { x: 3, y: 3 });
+      assert.strictEqual(line.length, 0);
     });
   });
   it('should give length of the line when both the points are not same and have floating value', function() {
-    let myLine = new Line({ x: 2, y: 3 }, { x: 3.5, y: 3 });
-    assert.strictEqual(myLine.length, 1.5);
+    let line = new Line({ x: 2, y: 3 }, { x: 3.5, y: 3 });
+    assert.strictEqual(line.length, 1.5);
 
-    myLine = new Line({ x: 2, y: 3 }, { x: 3.5, y: 4.5 });
-    assert.approximately(myLine.length, 2.1213, 0.0001);
+    line = new Line({ x: 2, y: 3 }, { x: 3.5, y: 4.5 });
+    assert.approximately(line.length, 2.1213, 0.0001);
   });
 });
 
-describe('slope property', function() {
+describe('slope', function() {
   it('should have slope for a line neither parallel to x nor to y axis', function() {
-    const myLine = new Line({ x: 3, y: 5 }, { x: 4, y: 6 });
-    assert.strictEqual(myLine.slope, 1);
+    const line = new Line({ x: 3, y: 5 }, { x: 4, y: 6 });
+    assert.strictEqual(line.slope, 1);
   });
   it('should have slope as -infinity if line is parallel to x axis ', function() {
-    const myLine = new Line({ x: 1, y: 2 }, { x: 3, y: 2 });
-    assert.strictEqual(myLine.slope, -Infinity);
+    const line = new Line({ x: 1, y: 2 }, { x: 3, y: 2 });
+    assert.strictEqual(line.slope, -Infinity);
   });
   it('should have slope as 0 if line is parallel to y axis ', function() {
-    const myLine = new Line({ x: 1, y: 2 }, { x: 1, y: 3 });
-    assert.strictEqual(myLine.slope, 0);
+    const line = new Line({ x: 1, y: 2 }, { x: 1, y: 3 });
+    assert.strictEqual(line.slope, 0);
   });
 });
 
--- test/testPoint.js
+++ test/testPoint.js
@@ -5,30 +5,30 @@ const Point = require('../src/point.js');
 describe('Point', function() {
   describe('toString', function() {
     it('should give string representation of point when it has x and y', function() {
-      const myPoint = new Point(1, 2);
-      assert.strictEqual(myPoint.toString(), '[Point @(1,2)]');
+      const point = new Point(1, 2);
+      assert.strictEqual(point.toString(), '[Point @(1,2)]');
     });
     it('should give string representation of point with undefined when x or y is not given', function() {
-      const myPoint = new Point(2);
-      assert.strictEqual(myPoint.toString(), '[Point @(2,undefined)]');
+      const point = new Point(2);
+      assert.strictEqual(point.toString(), '[Point @(2,undefined)]');
     });
   });
   describe('visit', function() {
     it('should give result of operation done on the function given', function() {
-      let myPoint = new Point(1, 2);
+      let point = new Point(1, 2);
       assert.strictEqual(
-        myPoint.visit((x, y) => x + y),
+        point.visit((x, y) => x + y),
         3
       );
-      myPoint = new Point(1, 2);
+      point = new Point(1, 2);
       assert.strictEqual(
-        myPoint.visit((x, y) => x * y),
+        point.visit((x, y) => x * y),
         2
       );
     });
     it('should give undefined if given is not a function', function() {
-      const myPoint = new Point(1, 2);
-      assert.strictEqual(myPoint.visit('function'), undefined);
+      const point = new Point(1, 2);
+      assert.strictEqual(point.visit('function'), undefined);
     });
   });
   describe('isEqualTo', function() {
@@ -36,6 +36,11 @@ describe('Point', function() {
       const point1 = new Point(1, 2);
       const point2 = new Point(1, 2);
       assert.isTrue(point1.isEqualTo(point2));
+    });
+    it('should say false if both points are not equal', function() {
+      const point1 = new Point(1, 2);
+      const point2 = new Point(2, 2);
+      assert.isFalse(point1.isEqualTo(point2));
     });
   });
 });
